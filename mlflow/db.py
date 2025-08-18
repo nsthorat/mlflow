b85d5df7c49c7ef2ef39171dfb96b31e83ab7d59
nsthorat <nsthorat@gmail.com>
2025-08-18 15:45:22 -0400

    Fix CLI imports after claude_code reorganization
    - Update CLI __init__.py to import traces from cli/, others from main mlflow/
    - Restore actual CLI implementations for db.py, experiments.py, runs.py
    - Remove backward compatibility imports that were breaking the CLI
    - CLI now works correctly: traces search, experiments, db, runs all functional
    
    Signed-off-by: nsthorat <nsthorat@gmail.com>

--- mlflow/db.py
+++ mlflow/db.py
@@ -1,3 +1,27 @@
-# Backward compatibility import for mlflow.db
-# This module has been moved to mlflow.cli.db
-from mlflow.cli.db import *  # noqa: F403
+import click
+
+
+@click.group("db")
+def commands():
+    """
+    Commands for managing an MLflow tracking database.
+    """
+
+
+@commands.command()
+@click.argument("url")
+def upgrade(url):
+    """
+    Upgrade the schema of an MLflow tracking database to the latest supported version.
+
+    **IMPORTANT**: Schema migrations can be slow and are not guaranteed to be transactional -
+    **always take a backup of your database before running migrations**. The migrations README,
+    which is located at
+    https://github.com/mlflow/mlflow/blob/master/mlflow/store/db_migrations/README.md, describes
+    large migrations and includes information about how to estimate their performance and
+    recover from failures.
+    """
+    import mlflow.store.db.utils
+
+    engine = mlflow.store.db.utils.create_sqlalchemy_engine_with_retry(url)
+    mlflow.store.db.utils._upgrade_db(engine)
